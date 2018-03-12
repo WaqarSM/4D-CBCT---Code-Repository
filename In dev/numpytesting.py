610a55e5e9bece690ff2894c2a301da60426d76a
Muhammad Waqar <>
2018-03-11 23:13:57 -0400

    Code to write new *SORT.txt file with custom number of bins

---
+++ In dev/numpytesting.py
@@ -0,0 +1,11 @@
+import csv
+import xml.etree.ElementTree as ET
+import numpy as np
+import array
+import shutil
+import os
+
+a = np.arange(4).reshape((2,2))
+a
+print np.amax(a)
+np.amax(a, axis=0)
